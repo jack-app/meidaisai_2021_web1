cb7737a538c7dd9669c520201df0b176a605afd6
H0R15H0 <shohori13@gmail.com>
2021-05-31 22:19:41 +0900

    Tweetモデルの説明追加

--- db/models.py
+++ db/models.py
@@ -3,8 +3,27 @@ from sqlalchemy.ext.hybrid import hybrid_property
 from db.database import Base, db_session
 from datetime import datetime
 
-
 class Tweet(Base):
+    '''
+    ~~~ 使い方 ~~~
+    1. まずはこのライブラリをインポート
+        # main.py
+        from db.models import Tweet
+    2. Tweet全部取得したい時
+        Tweet.all() # 配列で得られる。
+    3. Tweet作成したい時（データを保存したい時）
+        t = Tweet(name='名前', content='内容', position_x='座標X', position_y='座標Y') # インスタンス化
+        t.save() # 保存
+    
+    ~~~ 各属性たち ~~~
+    tweet.name            # (String型)   投稿者の名前
+    tweet.content         # (String型)   投稿内容
+    tweet.position_x      # (String型)   座標X
+    tweet.position_y      # (String型)   座標Y
+    tweet.created_at      # (datetime型) 投稿時刻
+    tweet.created_at_text # (String型)   投稿時刻
+    '''
+
     __tablename__ = 'tweets' 
     id = Column(Integer, primary_key=True)
     name = Column(String(128))
