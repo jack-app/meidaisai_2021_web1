6126ade06d5d3835bf0da4d85559fb2ce454ed4f
kyannkyann <amainuyama20339@outlook.jp>
2021-06-02 18:01:41 +0900

    day3を参考にしました

--- main.py
+++ main.py
@@ -7,7 +7,13 @@ def index():
 
 @app.route("/shikiso")
 def shikisoshikiso():
+    name = request.args.get("name")
     return render_template("akaishikiso.html")
+
+@app.route("/shikiso",methods=["post"])
+def post():
+    name = request.args.get("name")
+    return render_template("akaishikiso.html",name=name)
 
 if __name__ == "__main__" :
     app.run(debug=True,threaded=True)
